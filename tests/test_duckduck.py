a2cdd22dfaecef8e1138fd6c6d6937113e6efd9f
Andrei Arkhipov <it.arkhipov@gmail.com>
2024-01-14 13:14:07 +0300

    part 0

--- tests/test_duckduck.py
+++ tests/test_duckduck.py
@@ -7,13 +7,7 @@ from selenium.webdriver.chrome.service import Service as ChromeService
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.wait import WebDriverWait
 from webdriver_manager.chrome import ChromeDriverManager
-
-from bromium.browser import Browser
-from bromium.conditions import element, type, click, number_of_elements
-
-
-def find_element(*args):
-    return driver.find_element(By.CSS_SELECTOR, '#searchbox_input')
+from bromium.conditions import element
 
 
 driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()))
@@ -22,8 +16,12 @@ wait = WebDriverWait(driver, timeout=2, ignored_exceptions=(WebDriverException,)
 
 driver.get('https://duckduckgo.com')
 
-# driver.find_element(By.CSS_SELECTOR, '#searchbox_input').send_keys('bromium', Keys.ENTER)
-wait.until(find_element).send_keys('bromium', Keys.ENTER)
+wait.until(element(By.CSS_SELECTOR, '#searchbox_input')).send_keys('bromium', Keys.ENTER)
+
+wait.until(element(
+    By.CSS_SELECTOR, '.react-results--main li:nth-child(2) [data-testid="result-title-a"]')
+).click()
+
 time.sleep(3)
 
 driver.quit()
