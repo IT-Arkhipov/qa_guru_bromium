c84ed7030308436dd55f2936859bdc38cebd568f
Andrei Arkhipov <it.arkhipov@gmail.com>
2024-01-15 11:29:29 +0300

    refactor

--- tests/test_duckduck.py
+++ tests/test_duckduck.py
@@ -6,32 +6,11 @@ from selenium.webdriver.chrome.service import Service as ChromeService
 
 
 driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()))
-
-# 2
 browser = Browser(driver)
 
 browser.driver.get('https://duckduckgo.com')
 
-'''
-# in Selene:
-browser.element('[name=q]').type('selene').press_enter()
-# in Selenium WebDriver:
-driver.find_element(By.CSS_SELECTOR, '[name=q]').send_keys('selene', Keys.ENTER)
-# OR with wait
-def find_element(driver):
-    return driver.find_element(By.CSS_SELECTOR, '[name=q]')
-wait.until(find_element).send_keys('selene', Keys.ENTER)
-# OR with built-in expected condition
-wait.until(visibility_of_element_located((By.NAME, 'q'))).send_keys('selene yashaka', Keys.ENTER)
-# OR with custom expected condition
-wait.until(element('[name=q]')).send_keys('selene yashaka', Keys.ENTER)
-'''
-
 query = '[name=q]'
-# wait.until(type_request(query, value='selene' + Keys.ENTER))
-# 1
-# type_request(query, value='selene' + Keys.ENTER)
-# 2
 browser.type_request(query, value='selene' + Keys.ENTER)
 # 3
 # element('[name=q]').type('selene' + Keys.ENTER)
@@ -40,30 +19,12 @@ browser.type_request(query, value='selene' + Keys.ENTER)
 # query.type('selene' + Keys.ENTER)
 
 driver.back()
-
-# wait.until(type(query, value=' yashaka' + Keys.ENTER))
-# 1
-# type_request(query, ' yashaka' + Keys.ENTER)
-# 2
 browser.type_request(query, ' yashaka' + Keys.ENTER)
 # 3
 # query.type(' yashaka' + Keys.ENTER)
 
-# wait.until(click('.react-results--main>li:nth-of-type(1) [data-testid="result-title-a"]'))
-# 1
-# click('.react-results--main>li:nth-of-type(1) [data-testid="result-title-a"]')
-# 2
 browser.click('.react-results--main>li:nth-of-type(1) [data-testid="result-title-a"]')
 
-# wait.until(number_of_elements('.md-content img', value=13))
-# 1
-# assert_that('.md-content img', value=13)
-# 2
 browser.assert_that('.md-content img', value=13)
-'''
-# less stable version:
-number_of_pulls = len(driver.find_elements(By.CSS_SELECTOR, '[id^=issue_]:not([id$=_link])'))
-assert number_of_pulls == 4
-'''
 
 driver.quit()
